ce53da6736460e217257cc2e764c88a4f78735f5
Parth <shridharlondhe98@gmail.com>
2025-09-22 14:04:11 +0530

    Initial commit

---
+++ app/head.js
@@ -0,0 +1,14 @@
+export default function Head() {
+  return (
+    <>
+      <title>Parth Londhe | Web Developer</title>
+      <meta name="description" content="Portfolio of Parth Londhe - Web Developer and Tech Enthusiast" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="author" content="Parth Londhe" />
+      <meta property="og:title" content="Parth Londhe Portfolio" />
+      <meta property="og:description" content="Portfolio of Parth Londhe - Web Developer and Tech Enthusiast" />
+      <meta property="og:type" content="website" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
